433ace49bda57b989a049766340c2c5039f91f03
DorartZeqiri <dorart.zeqiri1@gmail.com>
2024-10-18 17:08:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from idlelib.sidebar import temp_enable_text_widget
+
+emri = "Dorart"
+mbiemri = "Zeqiri"
+mosha = 17
+temperatura = 25.5
+
+print(emri)
+print(type(emri))
+print(type(mosha))
+print(type(temperatura))
+
+mosha = mosha + 10
+
+print(mosha)
+emri_mbiemri = emri + " Rexha"
+
+print(emri_mbiemri)
+
+my_data = ["Dorart", "Zeqiri", "17", "Shkolla Digjitale"]
+
+emri = my_data [0]
+mbiemri = my_data[1]
+mosha = my_data [2]
+shkolla = my_data [3]
+print(my_data)
+my_data.append("Prishtina")
+print(my_data)
+my_data.insert(4, "Klasa12")
+print(my_data)
+
+my_data.remove('Dorart')
+
+print(my_data)
